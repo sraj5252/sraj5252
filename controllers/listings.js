57282d91706d7382154fda7229f5035d148a4640
Raj Singh <rajubieber80525@gmail.com>
2025-02-01 20:22:24 +0530

    changed the code in controller/listing.js  of mapGeocoding as function

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,5 +1,5 @@
 const Listing = require("../models/listing");
-const { mapGeocoding } = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapGeocoding  = require('@mapbox/mapbox-sdk/services/geocoding');
 const mapToken = process.env.MAP_TOKEN;
 const geocodingClient = mapGeocoding({ accessToken:mapToken});
 
